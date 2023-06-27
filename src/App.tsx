6e21cf559de7e3839f6483a64dce727ea28b4c5c
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-06-27 22:05:10 +0900

    useQueryのサンプル実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Response {
+  success: boolean;
+  data: {
+    name: string;
+    note: string;
+    age: number;
+    registerDate: string;
+  };
+}
+
+const getShakespeare = async () => {
+  const { data } = await axios.get<Response>(
+    "https://umayadia-apisample.azurewebsites.net/api/persons/Shakespeare"
+  );
+  return data;
+};
 
 function App() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["shakespeare"],
+    queryFn: getShakespeare,
+  });
+
+  const shakespeareEntries = useMemo(() => {
+    return data?.data ? Object.entries(data.data) : [];
+  }, [data?.data]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isLoading ? (
+          "loading"
+        ) : (
+          <img src={logo} className="App-logo" alt="logo" />
+        )}
+        {shakespeareEntries.map(([key, value]) => (
+          <p key={key}>
+            {key}: {value}
+          </p>
+        ))}
       </header>
     </div>
   );
