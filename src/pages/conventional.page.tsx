752d24059bed9d0b2df7a2e254662404789ddc4b
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-07-17 13:39:07 +0900

    ConventionalPageの実装

--- src/pages/conventional.page.tsx
+++ src/pages/conventional.page.tsx
@@ -1,26 +1,38 @@
-import { useEffect } from "react";
-import { getTannerlinsley } from "../api/getTannerlinsley";
+import { useEffect, useState } from "react";
+import { Response, getTannerlinsley } from "../api/getTannerlinsley";
 
 export const ConventionalPage: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setDate] = useState<Response>();
+  const [error, setError] = useState<Error>();
+
   useEffect(() => {
     const fetch = async () => {
-      await getTannerlinsley();
+      try {
+        setIsLoading(true);
+        const res = await getTannerlinsley();
+        setDate(res);
+      } catch (e) {
+        if (!(e instanceof Error)) return;
+        setError(e);
+      } finally {
+        setIsLoading(false);
+      }
     };
     fetch();
   }, []);
 
-  // if (isLoading || !data) return <p>Loading...</p>;
+  if (isLoading || !data) return <p>Loading...</p>;
 
-  // if (error instanceof Error)
-  //   return <p>An error has occurred: + {error.message}</p>;
+  if (error) return <p>An error has occurred: + {error.message}</p>;
 
   return (
     <div>
-      {/* <h1>{data.name}</h1>
+      <h1>{data.name}</h1>
       <p>{data.description}</p>
       <strong>👀 {data.subscribers_count}</strong>{" "}
       <strong>✨ {data.stargazers_count}</strong>{" "}
-      <strong>🍴 {data.forks_count}</strong> */}
+      <strong>🍴 {data.forks_count}</strong>
     </div>
   );
 };
